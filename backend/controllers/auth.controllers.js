861a662d06ee80c748732753e519a6a5c2e59876
ad-tobii <165202429+ad-tobii@users.noreply.github.com>
2025-12-03 04:38:19 +0100

    Otp verification debugging

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -242,9 +242,8 @@ export const sendVerificationEmail = async (req, res) => {
 export const verifyEmail = async (req, res) => {
   try {
     // Retrive user
-    const { userId } = req.user;
+    const userId = req.user._id;
     const user = await User.findById(userId);
-
     console.log('this user is trying to verify their email', user);
 
     // Check if the user is already verified
