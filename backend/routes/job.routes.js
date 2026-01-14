9a514a8e9c2259652f3340a1af19a187b1854e27
ad-tobii <165202429+ad-tobii@users.noreply.github.com>
2026-01-14 05:29:37 +0100

    feat : get Job route for job ui created

--- backend/routes/job.routes.js
+++ backend/routes/job.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createJob, listJobs } from '../controllers/job.controllers.js';
+import { createJob, listJobs,getJob } from '../controllers/job.controllers.js';
 import parser from '../utils/multer.utils.js';
 import protectRoutes from '../middleware/protectRoutes.middleware.js';
 
@@ -10,5 +10,6 @@ router.post('/post-job', protectRoutes, parser.array('images', 5), createJob);
 
 // Get all open jobs
 router.get('/get-jobs', protectRoutes, listJobs);
+router.get('/:jobId', protectRoutes, getJob);
 
 export default router;
